3227c681c5db3584c19c0c9c0aa53b01d2f31066
lgab1219 <lancegabrielmaghanoy@gmail.com>
2025-05-29 18:01:48 +0800

    added todo-list base with task removal feature

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import './App.css'
+
+function Form({ addTask, setTaskInput, taskInput  }) {
+
+  function handleAdd(event) {
+    event.preventDefault();
+    addTask();
+  }
+
+  return (
+    <>
+      <div className='task-container'>
+        <form className='task-form'>
+          <label htmlFor="task-input">Write your task below</label><br />
+          <input type="text" name="taskInput" id='task-input' value={taskInput} onChange={(e) => { setTaskInput(e.target.value) }}/>
+          <button type="submit" className='add-button' onClick={handleAdd}>Add Task</button>
+        </form>
+      </div>
+    </>
+  )
+}
+
+function App() {
+
+  const [tasks, setTask] = useState([]);
+  const [taskInput, setTaskInput] = useState('');
+
+  function addTask() {
+    setTask([...tasks, taskInput]);
+    setTaskInput('');
+  }
+
+  function removeTask(index) {
+    const updatedTasks = tasks.filter((_, i) => (i !== index));
+    setTask(updatedTasks);
+  }
+
+  return (
+    <>
+      <Form addTask={addTask} setTaskInput={setTaskInput} taskInput={taskInput} />
+      <p>Tasks:</p><br />
+      {tasks.map((task, index) => (
+        <div className='task' key={index}>
+          <p>{task}</p>
+          <input type="button" value="Completed" className='complete-box' onClick={() => {removeTask(index)}} />
+        </div>
+      ))}
+    </>
+  )
+}
+
+export default App
