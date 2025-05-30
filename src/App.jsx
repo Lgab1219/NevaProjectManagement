5741ce69dafb04b326d5bb236521fe4ee1c43679
lgab1219 <lancegabrielmaghanoy@gmail.com>
2025-05-30 18:02:16 +0800

    added simple styling to keep web app clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,46 @@
 import { useState } from 'react'
+import Form from './Form.jsx'
 import './App.css'
-
-function Form({ addTask, setTaskInput, taskInput  }) {
-
-  function handleAdd(event) {
-    event.preventDefault();
-    addTask();
-  }
-
-  return (
-    <>
-      <div className='task-container'>
-        <form className='task-form'>
-          <label htmlFor="task-input">Write your task below</label><br />
-          <input type="text" name="taskInput" id='task-input' value={taskInput} onChange={(e) => { setTaskInput(e.target.value) }}/>
-          <button type="submit" className='add-button' onClick={handleAdd}>Add Task</button>
-        </form>
-      </div>
-    </>
-  )
-}
 
 function App() {
 
   const [tasks, setTask] = useState([]);
   const [taskInput, setTaskInput] = useState('');
-  const [taskRemoved, setTaskRemoved] = useState(false);
 
-  function addTask() {
-    setTask([...tasks, taskInput]);
+  function addTask(event) {
+    const priorityValue = event.target.priority.value;
+    
+    const newTask = {
+      id: Date.now(),
+      text: taskInput,
+      priority: priorityValue,
+      completed: false
+    }
+
+    setTask([...tasks, newTask]);
     setTaskInput('');
   }
 
-  function removeTask(index) {
-    const updatedTasks = tasks.filter((_, i) => (i !== index));
+  function removeTask(taskID) {
+    setTask(tasks => tasks.map(task => task.id === taskID ? {...task, completed: !task.completed} : task));
+
+    setTimeout(() => {
+    const updatedTasks = tasks.filter(task => (task.id !== taskID));
     setTask(updatedTasks);
+    }, 1000);
+
   }
 
   return (
     <>
       <Form addTask={addTask} setTaskInput={setTaskInput} taskInput={taskInput} />
       <p>Tasks:</p><br />
-      {tasks.map((task, index) => (
-        <div className='task' key={index}>
+      {tasks.map(task => (
+        <div className={`task ${task.completed ? 'remove' : ''}`} key={task.id}>
           <h3>Task:</h3>
-          <p>{task}</p>
-          <input type="button" value="Completed" className='complete-btn' onClick={() => {removeTask(index)}} />
+          <p>{task.text}</p>
+          <p>Priority: {task.priority}</p>
+          <input type="button" value="Completed" className='complete-btn' onClick={() => {removeTask(task.id)}} />
         </div>
       ))}
     </>
