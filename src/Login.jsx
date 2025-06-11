49198fd96ef1e4f6aff48865687c991773bb8766
lgab1219 <lancegabrielmaghanoy@gmail.com>
2025-06-11 18:37:07 +0800

    improved styling and animation with color palette

--- src/Login.jsx
+++ src/Login.jsx
@@ -7,18 +7,35 @@ function Login() {
 
     return (
         <>
-            <div className="login-container">
-                <h1>Login</h1>
-                <form onSubmit={context.loginAccount}>
-                    <label htmlFor="email-input">Email</label><br />
-                    <input type="text" name="email" id="email-input" onChange={(e) => {context.setEmailInput(e.target.value)}} /><br /><br />
-
-                    <label htmlFor="password-input">Password</label><br />
-                    <input type="text" name="password" id="password-input" onChange={(e) => {context.setPasswordInput(e.target.value)}} /><br /><br />
-
-                    <button type="submit">Login</button>
-                </form>
+        <div>
+            <div className="login-bgtext">
+                LOGIN
             </div>
+
+           <div className="cover">
+
+           </div>
+
+            <main className="login-container d-block text-center rounded-4 inter z-1">
+                <span>
+                    <img src="../resources/NEVA_Logo.png" className="logo" alt="logo" />
+                </span>
+                <form className="login-form" onSubmit={context.loginAccount}>
+                    <div className="form-group">
+                        <label htmlFor="email-input">Email</label><br />
+                        <input type="text" name="email" id="email-input" onChange={(e) => {context.setEmailInput(e.target.value)}} /><br /><br />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="password-input">Password</label><br />
+                        <input type="text" name="password" id="password-input" onChange={(e) => {context.setPasswordInput(e.target.value)}} /><br /><br />
+                    </div>
+
+                    <button className="login-btn my-3 rounded-5 inter" type="submit">Login</button>
+                    <p>Want an account? <a href="" onClick={context.loginRegisterToggle}>Register</a> here!</p>
+                </form>
+            </main>
+        </div>
         </>
     )
 }
--- src/Register.jsx
+++ src/Register.jsx
@@ -7,17 +7,39 @@ function Register() {
 
     return (
         <>
-        <div className="register-container">
-            <h1>Register</h1>
-            <form onSubmit={context.registerAccount}>
-                <label htmlFor="user-input">Username</label><br />
-                <input type="text" name="username" id="user-input" onChange={(e) => {context.setUserInput(e.target.value)}} /><br /><br />
-                <label htmlFor="email-input">Email</label><br />
-                <input type="text" name="email" id="email-input" onChange={(e) => {context.setEmailInput(e.target.value)}} /><br /><br />
-                <label htmlFor="password-input">Password</label><br />
-                <input type="text" name="password" id="password-input" onChange={(e) => {context.setPasswordInput(e.target.value)}} /><br /><br />
-                <button type="submit">Register</button>
-            </form>
+        <div>
+            <div className="cover-reverse">
+
+            </div>
+
+           <div className="register-bgtext">
+            REGISTER
+           </div>
+
+            <main className="register-container d-block text-center rounded-4 inter z-1">
+                <span>
+                    <img src="../resources/NEVA_Logo.png" className="logo" alt="logo" />
+                </span>
+                <form onSubmit={context.registerAccount}>
+                    <div className="form-group">
+                        <label htmlFor="user-input">Username</label><br />
+                        <input type="text" name="username" id="user-input" onChange={(e) => {context.setUserInput(e.target.value)}} /><br /><br />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="email-input">Email</label><br />
+                        <input type="text" name="email" id="email-input" onChange={(e) => {context.setEmailInput(e.target.value)}} /><br /><br />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="password-input">Password</label><br />
+                        <input type="text" name="password" id="password-input" onChange={(e) => {context.setPasswordInput(e.target.value)}} /><br /><br />
+                    </div>
+
+                    <button className="login-btn my-3 rounded-3 inter" type="submit">Register</button>
+                    <p>Already have an account? <a href="" onClick={context.loginRegisterToggle}>Login</a> here!</p>
+                </form>
+            </main>
         </div>
         </>
     )
