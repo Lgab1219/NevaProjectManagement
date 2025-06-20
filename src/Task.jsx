3a96c68e41167615fdf73e93f602f1feabe1bbb1
lgab1219 <lancegabrielmaghanoy@gmail.com>
2025-06-20 15:37:21 +0800

    refactored code by adding React Context for prop handling

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,14 +1,18 @@
+import UserContext from './js/UserContext'
+import { useContext } from 'react'
 import './css/App.css'
 
-function Task({ tasks = [], removeTask }) {
+function Task() {
+
+    const context = useContext(UserContext);
 
     return (
         <>
-            {tasks.map(task => (
+            {context.tasks.map(task => (
                 <div className={`task ${task.completed ? 'remove' : ''}`} key={task.id}>
                   <h3>{task.text}</h3>
                   <p>Priority: {task.priority}</p>
-                  <input type="button" value="Completed" className='complete-btn' onClick={() => {removeTask(task.id)}} />
+                  <input type="button" value="Completed" className='complete-btn' onClick={() => {context.removeTask(task.id)}} />
                 </div>
             ))}
         </>
